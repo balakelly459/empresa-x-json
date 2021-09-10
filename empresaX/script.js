918bf7c87b1e0b4c3706119290865d5700d50661
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-09-10 10:15:00 -0300

    ajustes

--- empresaX/script.js
+++ empresaX/script.js
@@ -3,7 +3,11 @@ function abrirCadastro() {
     document.getElementById("cadastrar__area").style.display = "flex";
 }
 
+function fecharCadastro() {
+    document.getElementById("cadastrar__area").style.display = "none"
+}
+
 document.getElementById("button__cadastrar").addEventListener("click", abrirCadastro)
 
-// document.getElementById("button__cancelar").addEventListener("click", fecharCadastro)
+document.getElementById("button__cancelar").addEventListener("click", fecharCadastro)
 
